5bbac683fb92945d9ff4170c4bdaf6239dc983ad
Xiang ZHU <xiang.zhu@outlook.com>
2019-07-28 02:48:12 +0200

    add initial setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="microservice-flask",
+    version="0.1",
+    description="microservice-flask by learning from the book Python Microservices Development",
+    url="https://github.com/copdips/microservice-flask",
+    author="Xiang ZHU",
+    author_email="xiang.zhu@outlook.com",
+    license="MIT",
+    packages=[],
+    install_requires=["flask", "blinker"],
+    zip_safe=False,
+)
